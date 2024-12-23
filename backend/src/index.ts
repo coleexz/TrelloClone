0c22a364f20d3b815c70d919ee1f866ae8d67bce
coleexz <williamstevencole@gmail.com>
2024-12-23 15:24:48 -0600

    Setup de docker con backend y postgres como base de datos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,7 @@ const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
