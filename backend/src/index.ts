0d0bc89383d70d4df47711ed2b6f480049ef8a15
coleexz <williamstevencole@gmail.com>
2024-12-23 14:42:10 -0600

    Setup del backend junto con algunas dependencias

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
